75d4f2deee5bd91786e7a92b4d7400232f963de9
Jack <jack@Toadkiller.(none)>
2013-12-06 01:04:18 -0800

    Added some new files.

---
+++ laserpony/models.py
@@ -0,0 +1,68 @@
+import datetime
+
+from flask_login import AnonymousUserMixin
+from util import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Post(db.Document):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    title = db.StringField(max_length=255, required=True)
+    slug = db.StringField(max_length=255, required=True)
+    body = db.StringField(required=True)
+
+"""
+class User(db.Document):
+    name = db.StringField(max_length=64, required=True)
+    email = db.StringField(max_length=254)
+    password_hash = db.StringField()
+    authenticated = db.BoolField()
+    active = db.BoolField()
+    admin = db.BoolField()
+
+    def __init__(self, name, email, password,
+                active=False, admin=False, authenticated=False):
+        password_hash = generate_password_hash(password)
+
+        super(User, self).__init__(
+            name=name,
+            email=email,
+            password_hash=password_hash,
+            active=active,
+            admin=admin,
+            authenticated=authenticated
+        )
+
+    def check_password(self, password):
+            return check_password_hash(self.password, password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def is_active(self):
+        return self.active
+
+    def is_admin(self):
+        return self.admin
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def get_id(self):
+        return self.name
+
+
+class Anonymous(AnonymousUserMixin):
+    def is_admin(self):
+        return False
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.filter(User.name == userid).first()
+
+login_manager.anonymous_user = Anonymous
+"""
+
