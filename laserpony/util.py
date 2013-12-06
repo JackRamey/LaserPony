e6cf4bea4a3dde0fa2a52790adcfffd360ab79a7
Jack <jack@Toadkiller.(none)>
2013-12-06 01:08:03 -0800

    Added Markdown to post body content

--- laserpony/util.py
+++ laserpony/util.py
@@ -1,10 +1,14 @@
 from laserpony import app
 from flask_mongoengine import MongoEngine
 from flask_login import LoginManager
+from flask.ext.markdown import Markdown
 
 #Database
 db = MongoEngine(app)
 
 #Login Manager
 login_manager = LoginManager(app)
+
+#Markdown Parser
+Markdown(app)
 
