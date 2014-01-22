bb242d6346b98f60c7fd2e05cab76c0c6872ff1f
Jack <jack.ramey@gmail.com>
2014-01-22 10:28:09 -0800

    Added badges to projects nav

--- laserpony/models/project.py
+++ laserpony/models/project.py
@@ -6,3 +6,9 @@ class Project(db.Document):
     name = db.StringField(max_length=25, required=True)
     slug = db.StringField(max_length=25, required=True)
 
+    def postCount(self):
+        #Import here to prevent circular imports
+        from laserpony.models.post import Post
+        posts = Post.objects(project=self)
+        return len(posts)
+
